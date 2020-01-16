a7c56a60d8519b43d81b59fa49d475b85bed4c4e
Hamza Tekin <58397806+Glock-Atom@users.noreply.github.com>
2020-01-16 16:11:39 +0100

    Add files via upload

---
+++ Lauflicht_2.ino
@@ -0,0 +1,28 @@
+int LED[] = {2,3,4,5,6};
+
+
+void setup(){
+   for (int i = 0;i<5; i ++){
+     pinMode(LED[i], OUTPUT);
+}
+}
+  
+
+
+void loop() {  
+  for (int i = 0; i <5; i ++)  {      
+    
+    digitalWrite(LED[i], HIGH);
+    digitalWrite(LED[4-i], HIGH);    
+    delay(600- i*100); 
+    digitalWrite(LED[i], LOW);
+    digitalWrite(LED[4-i], LOW);  
+    
+    
+  
+     
+  
+} 
+}
+  
+  
